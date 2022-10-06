e6ffdeb379109139e38a571b2650387fedf74c6d
Eric McKinney <giveittogod27@gmail.com>
2022-10-06 15:34:03 -0500

    Lesson 2 - My First Application

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log('Hello World');
+
+// Single Line comment
+
+/** Multi-Line 
+ * Comments
+ */
+
+let variableName = 'Hello';
+
+console.log(variableName);
+
+variableName = 10;
+
+console.log(variableName);
