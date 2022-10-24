e6e0cc2a5545baa0096b0ec23367aa4243e8b657
Eric McKinney <giveittogod27@gmail.com>
2022-10-24 00:19:53 -0500

    Numbers Lab

---
+++ labs/numberslab.js
@@ -0,0 +1,23 @@
+/* Numbers lab */
+
+// Numeric Separators
+let aa = 34_038
+let bb = 45_838.8734
+let cc = 0b011_000_101_011
+let dd = 0x1_74_C0
+let ee = -895_612
+
+console.log(aa);
+console.log(bb);
+console.log(cc);
+console.log(dd);
+console.log(ee);
+
+
+// Number Systems
+let ex_binary = 0b11101; // binary number
+let ex_hexadecimal = 0x1D; // hexadcimal number
+let ex_octal = 0o35;
+
+console.log(`Binary ${ex_binary}, Hexidecimal ${ex_hexadecimal}
+Octal ${ex_octal}`);
