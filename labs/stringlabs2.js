ecdb9268713dfb009bdc4db720a663d1a1fef2e6
Eric McKinney <giveittogod27@gmail.com>
2022-11-04 17:40:57 -0500

    Strings Lab

---
+++ labs/stringlabs2.js
@@ -0,0 +1,12 @@
+/* Strings lab assignment */
+
+let msg = " This statement has empty spaces ";
+
+console.log(msg.includes());
+console.log(msg.at());
+console.log(msg.trim());
+console.log(msg.toLowerCase());
+
+
+let phrase = "Jack and Jill went up the hill\n to fetch a pail of water.\n Jack fell down and broke his crown\n and Jill came tumbling after.";
+console.log(phrase);
